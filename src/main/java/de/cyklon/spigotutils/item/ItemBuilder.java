c963bbc6f35918dc33ff27d673bb2a1c3dbeaad2
cyklon73 <cyklon698@gmail.com>
2023-10-15 15:32:59 +0200

    improved ItemBuilder

--- src/main/java/de/cyklon/spigotutils/item/ItemBuilder.java
+++ src/main/java/de/cyklon/spigotutils/item/ItemBuilder.java
@@ -7,8 +7,11 @@ import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ItemBuilder {
@@ -16,21 +19,25 @@ public class ItemBuilder {
     private ItemMeta meta;
     private ItemStack item;
 
-    public ItemBuilder(Material material) {
-        this(new ItemStack(material));
+    public ItemBuilder(@NotNull Material material) {
+        this(material, 1);
     }
 
-    public ItemBuilder(ItemStack stack) {
+    public ItemBuilder(@NotNull Material material, int amount) {
+        this(new ItemStack(material, amount));
+    }
+
+    public ItemBuilder(@NotNull ItemStack stack) {
         item = stack;
         meta = item.getItemMeta();
     }
 
-    public ItemBuilder setDisplayName(String displayName) {
+    public ItemBuilder setDisplayName(@Nullable String displayName) {
         meta.setDisplayName(displayName);
         return this;
     }
 
-    public ItemBuilder setLocalizedName(String localizedName) {
+    public ItemBuilder setLocalizedName(@Nullable String localizedName) {
         meta.setLocalizedName(localizedName);
         return this;
     }
@@ -40,26 +47,36 @@ public class ItemBuilder {
         return this;
     }
 
-    public ItemBuilder addItemFlags(ItemFlag flag) {
-        meta.addItemFlags(flag);
+    public ItemBuilder addItemFlags(@NotNull ItemFlag... flags) {
+        meta.addItemFlags(flags);
         return this;
     }
 
-    public ItemBuilder setCustomModelData(Integer data) {
+    public ItemBuilder setCustomModelData(@Nullable Integer data) {
         meta.setCustomModelData(data);
         return this;
     }
 
-    public ItemBuilder setLore(List<String> lore) {
+    public ItemBuilder setLore(@Nullable List<String> lore) {
         meta.setLore(lore);
         return this;
     }
 
-    public ItemBuilder addLore(List<String> lore) {
+    public ItemBuilder setLore(@NotNull String... lore) {
+        return setLore(Arrays.asList(lore));
+    }
+
+    public ItemBuilder addLore(@Nullable List<String> lore) {
         List<String> oldLore = meta.getLore();
+        if (oldLore==null && lore==null) return setLore((List<String>) null);
+        if (lore==null) return this;
         if (oldLore==null) oldLore = new ArrayList<>();
         oldLore.addAll(lore);
         return setLore(oldLore);
+    }
+
+    public ItemBuilder addLore(@NotNull String... lore) {
+        return addLore(Arrays.asList(lore));
     }
 
     public ItemBuilder removeLore(String regex) {
@@ -68,8 +85,8 @@ public class ItemBuilder {
         return this;
     }
 
-    public ItemBuilder addEnchantment(Enchantment ench, Integer level) {
-        meta.addEnchant(ench, level, true);
+    public ItemBuilder addEnchantment(Enchantment enchantment, Integer level) {
+        meta.addEnchant(enchantment, level, true);
         return this;
     }
 
@@ -78,7 +95,7 @@ public class ItemBuilder {
         return this;
     }
 
-    public <T, Z> ItemBuilder setPersistentDataContainer(NamespacedKey key, PersistentDataType<T, Z> type, Z value) {
+    public <T, Z> ItemBuilder setPersistentData(NamespacedKey key, PersistentDataType<T, Z> type, Z value) {
         meta.getPersistentDataContainer().set(key, type, value);
         return this;
     }
