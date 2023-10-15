5c9035ab155808ea060e9a2b6c5edc268abec9b5
cyklon73 <cyklon698@gmail.com>
2023-10-15 18:37:56 +0200

    set the icon to custom player heads

--- src/main/java/de/cyklon/spigotutils/ui/component/GuiNextPageButton.java
+++ src/main/java/de/cyklon/spigotutils/ui/component/GuiNextPageButton.java
@@ -1,6 +1,7 @@
 package de.cyklon.spigotutils.ui.component;
 
 import de.cyklon.spigotutils.item.ItemBuilder;
+import de.cyklon.spigotutils.item.SkullManager;
 import org.bukkit.Material;
 
 /**
@@ -9,7 +10,7 @@ import org.bukkit.Material;
 public class GuiNextPageButton extends GuiButton {
 
     public GuiNextPageButton() {
-        super(new ItemBuilder(Material.LAVA_BUCKET).setDisplayName("Weiter").build(), (gui, p) -> {
+        super(new ItemBuilder(SkullManager.OAK_WOOD_ARROW_RIGHT).setDisplayName("Weiter").build(), (gui, p) -> {
             if (gui.getCurrentPage()<gui.getPageAmount()) gui.setCurrentPage(gui.getCurrentPage()+1);
         });
     }
