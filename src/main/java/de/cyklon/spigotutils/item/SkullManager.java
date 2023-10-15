46f354d8f72a923187e496345956c8266783ae8a
cyklon73 <cyklon698@gmail.com>
2023-10-15 18:37:22 +0200

    SkullManager documented

--- src/main/java/de/cyklon/spigotutils/item/SkullManager.java
+++ src/main/java/de/cyklon/spigotutils/item/SkullManager.java
@@ -18,6 +18,11 @@ public class SkullManager {
     public static final ItemStack OAK_WOOD_ARROW_LEFT = getCustomHead("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYmQ2OWUwNmU1ZGFkZmQ4NGU1ZjNkMWMyMTA2M2YyNTUzYjJmYTk0NWVlMWQ0ZDcxNTJmZGM1NDI1YmMxMmE5In19fQ==");
     public static final ItemStack OAK_WOOD_ARROW_RIGHT = getCustomHead("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMTliZjMyOTJlMTI2YTEwNWI1NGViYTcxM2FhMWIxNTJkNTQxYTFkODkzODgyOWM1NjM2NGQxNzhlZDIyYmYifX19");
 
+    /**
+     * get the player head of a specific player
+     * @param player the player
+     * @return the player head
+     */
     public static ItemStack getPlayerHead(@NotNull OfflinePlayer player) {
         ItemStack stack = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta meta = (SkullMeta) stack.getItemMeta();
@@ -27,10 +32,22 @@ public class SkullManager {
         return stack;
     }
 
+    /**
+     * get a custom player head from a head url
+     * @param url the head url
+     * @return the custom head
+     */
     public static ItemStack getCustomHeadFromUrl(String url) {
         return getCustomHead(new String(Base64.getEncoder().encode(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).getBytes())));
     }
 
+    /**
+     * get a custom player head from the texture value
+     * <p>
+     * you can get the value from different websites, as example: https://minecraft-heads.com/custom-heads/
+     * @param base64 the texture value
+     * @return the custom player head
+     */
     public static ItemStack getCustomHead(@NotNull String base64) {
         ItemStack stack = new ItemStack(Material.PLAYER_HEAD);
 
