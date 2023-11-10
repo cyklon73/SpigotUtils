51b99d76bc744cb10e42464e69f8d819b6c77771
cyklon73 <cyklon698@gmail.com>
2023-11-10 22:18:50 +0100

    implemented CommandApi

---
+++ src/main/java/de/cyklon/spigotutils/command/annotation/Command.java
@@ -0,0 +1,24 @@
+package de.cyklon.spigotutils.command.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Command {
+
+    String name();
+
+    String prefix() default "";
+
+    String description() default "";
+
+    String usage() default "";
+
+    String[] aliases() default {};
+
+    String permission() default "";
+
+}
